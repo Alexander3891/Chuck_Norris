0c8818e4b07c8114f788bf080e50e6adaba7a9fa
Alexander Skrypnyk <62648298+Alexander3891@users.noreply.github.com>
2022-01-14 21:32:59 +0200

    Add files via upload

---
+++ src/Footer/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+
+    //     }
+    //     }
+    render() {
+        return (
+            <footer>
+                <div className="#"></div>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
+
+
+
+
+
